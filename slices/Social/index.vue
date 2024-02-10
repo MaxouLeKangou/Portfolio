d6608b2013077a28ce94eb1838a85114bf5d1c16
Maxime LEGRAND <maxime.legrand111@outlook.com>
2024-02-10 22:21:32 +0100

    feat. Slice SOCIAL

--- slices/Social/index.vue
+++ slices/Social/index.vue
@@ -1,23 +1,28 @@
 <script setup lang="ts">
-import { type Content } from "@prismicio/client";
+    import { type Content } from "@prismicio/client";
 
-// The array passed to `getSliceComponentProps` is purely optional.
-// Consider it as a visual hint for you when templating your slice.
-defineProps(
-  getSliceComponentProps<Content.SocialSlice>([
-    "slice",
-    "index",
-    "slices",
-    "context",
-  ]),
-);
+    // The array passed to `getSliceComponentProps` is purely optional.
+    // Consider it as a visual hint for you when templating your slice.
+    defineProps(
+        getSliceComponentProps<Content.SocialSlice>([
+            "slice",
+            "index",
+            "slices",
+            "context",
+        ]),
+    );
 </script>
 
 <template>
-  <section
-    :data-slice-type="slice.slice_type"
-    :data-slice-variation="slice.variation"
-  >
-    Placeholder component for social (variation: {{ slice.variation }}) Slices
-  </section>
+    <section :data-slice-type="slice.slice_type" :data-slice-variation="slice.variation">
+        <div class="hidden lg:block fixed z-20 bottom-24 left-24">
+            <ul class="flex flex-col gap-3">
+                <li v-for="(item, index) in slice.items" :key="index" class="flex">
+                    <PrismicLink :field="item.link" class="">
+                        <PrismicImage :field="item.image" class="w-7 h-7 m-3"/>
+                    </PrismicLink>
+                </li>
+            </ul>
+        </div>
+    </section>
 </template>
