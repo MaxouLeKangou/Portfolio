541ee7eca2d070a9f6b864977b9c7486969099ac
Maxime LEGRAND <maxime.legrand111@outlook.com>
2024-02-10 22:39:38 +0100

    feat. Favicon & metadescription

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,6 +11,22 @@ export default defineNuxtConfig({
     },
   },
 
+  app: {
+    head: {
+      title: 'Maxime Legrand - Portfolio',
+      htmlAttrs: {
+        lang: 'en',
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: "Découvrez le portfolio de Maxime LEGRAND, un créatif développeur web passionné par la création d'expériences visuelles mémorables et de solutions numériques innovantes. Explorez une sélection variée de mes meilleurs travaux, reflétant mon expertise en conception UX/UI, développement front-end, ... Basé à Montbéliard, je suis prêt à transformer vos idées en réalités visuelles captivantes." },
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    }
+  },
+
   prismic: {
     endpoint: "portfolio-bessarion"
   }
