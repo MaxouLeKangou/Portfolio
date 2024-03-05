8a62bf4c82408fe21eb681e4918b611dedd6ae53
Maxime LEGRAND <maxime.legrand111@outlook.com>
2024-03-05 16:19:25 +0100

    fix. top sticky WORKS slice

--- slices/Works/index.vue
+++ slices/Works/index.vue
@@ -29,7 +29,7 @@
           <div class="grid grid-cols-6 lg:grid-cols-12 gap-3 lg:gap-5">
             <PrismicRichText :field="slice.primary.title" class="uppercase sticky top-10 lg:top-24 tracking-[6px] mb-5 lg:mb-10 flex col-span-6 lg:col-span-12 justify-center text-sm text-center sm:text-lg lg:text-xl font-semibold whitespace-nowrap"/>
             <div class="flex flex-col gap-40 mx-4 sm:mx-12 col-span-6 lg:col-span-8 lg:col-start-3 lg:mx-0">
-              <section v-for="work in documents" :key="work.uid" class="sticky top-24 lg:top-4">
+              <section v-for="work in documents" :key="work.uid" class="sticky top-24 lg:top-48">
                 <NuxtLink :to="`/work/${work.uid}`" class="block h-[70vh]">
                   <div class="absolute z-10 flex flex-col gap-3 bottom-10 left-5 sm:left-10 sm:gap-4 lg:flex-row lg:items-end lg:justify-between lg:right-10 2xl:left-20 2xl:bottom-20 2xl:right-20">
                     <PrismicRichText :field="work.data.name" class="uppercase font-anton text-5xl sm:text-6xl 2xl:text-8xl"/>
