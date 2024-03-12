91f9b45042ecc7044f3fe6d468b35fbc5ec3d5e4
Maxime LEGRAND <maxime.legrand111@outlook.com>
2024-03-12 15:58:41 +0100

    fix. tab size

--- components/intro.vue
+++ components/intro.vue
@@ -1,47 +1,43 @@
 <script setup lang="ts">
     const isVisible = ref(false);
+    const slideOut = ref(false);
 
-// Variable pour contrôler si l'animation de slide-out doit être jouée.
-const slideOut = ref(false);
+    const greetings = [
+        "Bonjour", "Hello", "Hola", "Hallo", "Ciao", "こんにちは", 
+        "안녕하세요", "你好", "Olá", "Привет", "Merhaba", "Salam", 
+        "नमस्ते", "Hej", "Hei", "Tere", "Sveiki", "Labas", 
+        "Sziasztok", "Ahoj", "Sawubona"
+    ];
 
-const greetings = [
-  "Bonjour", "Hello", "Hola", "Hallo", "Ciao", "こんにちは", 
-  "안녕하세요", "你好", "Olá", "Привет", "Merhaba", "Salam", 
-  "नमस्ते", "Hej", "Hei", "Tere", "Sveiki", "Labas", 
-  "Sziasztok", "Ahoj", "Sawubona"
-];
+    const currentGreeting = ref(greetings[0]);
+    let greetingIndex = 0;
 
-const currentGreeting = ref(greetings[0]);
-let greetingIndex = 0;
+    function completeIntro() {
+        slideOut.value = true;
+        setTimeout(() => {
+            isVisible.value = false;
+            sessionStorage.setItem('hasSeenIntro', 'true');
+        }, 1000);
+    }
 
-// Cette fonction s'exécute une fois que toutes les salutations ont été affichées.
-function completeIntro() {
-  slideOut.value = true; // Commence l'animation de slide-out.
-  setTimeout(() => {
-    isVisible.value = false; // Cache l'écran d'introduction après l'animation.
-    sessionStorage.setItem('hasSeenIntro', 'true'); // Marque l'intro comme vue.
-  }, 1000); // Assurez-vous que ce délai correspond à la durée de votre animation.
-}
+    onMounted(() => {
+        if (!sessionStorage.getItem('hasSeenIntro')) {
+            isVisible.value = true;
+            const intervalId = setInterval(() => {
+                greetingIndex++;
+                if (greetingIndex >= greetings.length) {
+                    clearInterval(intervalId);
+                    completeIntro();
+                } else {
+                    currentGreeting.value = greetings[greetingIndex];
+                }
+            }, 200);
 
-onMounted(() => {
-  // Vérifie si l'intro a déjà été vue dans cette session.
-  if (!sessionStorage.getItem('hasSeenIntro')) {
-    isVisible.value = true;
-    const intervalId = setInterval(() => {
-      greetingIndex++;
-      if (greetingIndex >= greetings.length) {
-        clearInterval(intervalId);
-        completeIntro();
-      } else {
-        currentGreeting.value = greetings[greetingIndex];
-      }
-    }, 200); // Délai entre chaque salutation.
-
-    onUnmounted(() => {
-      clearInterval(intervalId);
+            onUnmounted(() => {
+                clearInterval(intervalId);
+            });
+        }
     });
-  }
-});
 </script>
 
 <template>
